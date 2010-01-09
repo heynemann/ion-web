01b966cea0450d5f1be9e0e242600828473a72a0
Bernardo Heynemann <heynemann@stormwindproject.org>
2010-01-09 12:47:10 -0200

    Version changed

--- ion/__init__.py
+++ ion/__init__.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-Version="0.1.0"
+Version="0.2.0"
 
 from context import *
 from bus import *
