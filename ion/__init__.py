1fceaaa56630b438b5162e1de51f87126ce7af8e
Bernardo Heynemann <bernardo@corp.globo.com>
2010-02-08 15:19:25 -0200

    Debian release

--- ion/__init__.py
+++ ion/__init__.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-Version="0.3.0"
+Version="0.3.1"
 
 from context import *
 from bus import *
