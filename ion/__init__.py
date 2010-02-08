25fe81306e843821d2b9ba36588fbe52a08c62db
Bernardo Heynemann <bernardo@corp.globo.com>
2010-02-08 15:44:54 -0200

    New version

--- ion/__init__.py
+++ ion/__init__.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-Version="0.3.1"
+Version="0.3.2"
 
 from context import *
 from bus import *
