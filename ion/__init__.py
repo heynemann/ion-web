6a0b49e3356b93651ecf056a0cd354e473235750
Bernardo Heynemann <heynemann@gmail.com>
2010-05-31 17:59:50 -0300

    Release 0.6.3

--- ion/__init__.py
+++ ion/__init__.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-Version="0.6.2"
+Version="0.6.3"
 version = Version
 __version__ = Version
 
